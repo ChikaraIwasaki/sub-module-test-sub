e0c22e9ead6aa2ebeefd741c660a5bb637c98aae
iwasa 岩﨑 勢(ＢＰ 営業支援部) <z102515@auto2.subaru.net>
2019-03-28 16:36:46 +0900

    mod

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @RequestMapping("/")
     String hello() {
-        return "hello world feature_a_01_modifyed";
+        return "hello world sub master";
     }
 }
